7f3c4eb014c8a82a3ffeb0db1838a1cb38ce93ca
Wes Gilleland <wgillela@opentext.com>
2019-09-27 12:13:01 -0400

    Remove home-brewed config stuff

--- VCS.psm1
+++ VCS.psm1
@@ -1,11 +1,3 @@
-$ModuleName = ([io.fileinfo]$MyInvocation.MyCommand.Definition).BaseName
-$ConfigPath = Join-Path (Join-Path $env:USERPROFILE ".config") $ModuleName
-if (!(Test-Path $ConfigPath)) {
-    New-Item -ItemType Directory -Path $ConfigPath | Out-Null
-}
-if ($Null -eq (Get-Variable "${ModuleName}ConfigLocation" -ErrorAction SilentlyContinue)) {
-    New-Variable -Name "${ModuleName}ConfigLocation" -Description "The where $ModuleName configs are stored"  -Value ($ConfigPath -as [IO.DirectoryInfo]) -Scope Script
-}
 $Functions = @{
     Public  = Get-ChildItem "$PSScriptRoot\Functions\Public\*.ps1" -File
     Private = Get-ChildItem "$PSScriptRoot\Functions\Private\*.ps1" -File
